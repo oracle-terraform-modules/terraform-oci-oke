20738e4cdc532d630e2d51b44c3100f6dc2a94cd
Ali Mukadam <ali.mukadam@oracle.com>
2019-04-02 14:05:11 +1100

    shebang line has to be first line

--- modules/base/bastion/scripts/bastion.template.sh
+++ modules/base/bastion/scripts/bastion.template.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
+
 # Copyright 2017, 2019, Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl
-
-#!/bin/bash
 
 yum install yum-plugin-security
 
