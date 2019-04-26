1aa8b1e7eb9e9cdeb6cb5687d99007df3f1c8184
Ali Mukadam <ali.mukadam@oracle.com>
2019-04-26 22:43:50 +1000

    bastion hardening improvement and calico update

--- modules/oke/scripts/install_calico.template.sh
+++ modules/oke/scripts/install_calico.template.sh
@@ -8,15 +8,17 @@ cd calico
 
 kubectl create clusterrolebinding clusteradminrole --clusterrole=cluster-admin --user=${user_ocid}
 
-kubectl apply -f https://docs.projectcalico.org/v${calico_version}/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-
 curl https://docs.projectcalico.org/v${calico_version}/getting-started/kubernetes/installation/hosted/kubernetes-datastore/policy-only/1.7/calico.yaml -O
+
+sed -i -e "s?192.168.0.0/16?${pod_cidr}?g" calico.yaml
 
 sleep 10
 
-if [ '${number_of_nodes}' > 50 ]; then
+if [ ${number_of_nodes} -gt 50 ]; then
   echo "More than 50 nodes detected. Setting the typha service name"
   sed -i -e 's/typha_service_name:\s"none"/typha_service_name: calico-typha/g' calico.yaml
+  kubectl apply -f calico.yaml
+  kubectl -n kube-system scale --current-replicas=1 --replicas=${number_of_replicas} deployment/calico-typha
+else
+  kubectl apply -f calico.yaml
 fi
-
-kubectl apply -f calico.yaml
