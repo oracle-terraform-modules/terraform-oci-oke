28e98d8e0fa30f0ca1eb8278517b5dd275d04708
Christopher Williamson <christopher@previ.se>
2019-05-03 15:08:37 +0100

    Cleaned up logic for fetching latest endpoint

--- modules/oke/outputs.tf
+++ modules/oke/outputs.tf
@@ -1,3 +1,3 @@
 output "cluster_endpoint" {
-  value = "${oci_containerengine_cluster.k8s_cluster.endpoints.*.kubernetes[count.index]}"
+  value = "${oci_containerengine_cluster.k8s_cluster.endpoints.0.kubernetes}"
 }
