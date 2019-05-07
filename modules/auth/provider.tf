24e9b551fa0c1db15a7f0c1c3dbf6667041adf75
Christopher Williamson <christopher@previ.se>
2019-05-07 14:55:04 +0100

    Removed several hard refs to the oci provider

--- modules/auth/provider.tf
+++ modules/auth/provider.tf
@@ -4,9 +4,5 @@
 # create a home region provider for identity operations
 provider "oci" {
   alias            = "home"
-  fingerprint      = "${var.api_fingerprint}"
-  private_key_path = "${var.api_private_key_path}"
   region           = "${var.home_region}"
-  tenancy_ocid     = "${var.tenancy_ocid}"
-  user_ocid        = "${var.user_ocid}"
 }
--- modules/base/iam.tf
+++ modules/base/iam.tf
@@ -16,11 +16,7 @@ data "oci_identity_regions" "home_region" {
 # create a home region provider for identity operations
 provider "oci" {
   alias            = "home"
-  fingerprint      = "${var.api_fingerprint}"
-  private_key_path = "${var.api_private_key_path}"
   region           = "${lookup(data.oci_identity_regions.home_region.regions[0], "name")}"
-  tenancy_ocid     = "${var.tenancy_ocid}"
-  user_ocid        = "${var.user_ocid}"
 }
 
 data "oci_identity_compartments" "compartments_name" {
