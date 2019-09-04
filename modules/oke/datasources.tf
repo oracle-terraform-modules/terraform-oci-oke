0d432e3d8f75ae9512951dd43b009abd621c9766
karthic <karthicgit>
2019-09-04 11:00:07 +0530

    changes in datasources.tf for nodeshape

--- modules/oke/datasources.tf
+++ modules/oke/datasources.tf
@@ -5,8 +5,9 @@ data "oci_core_images" "latest_images" {
   compartment_id           = var.oke_identity.compartment_ocid
   operating_system         = var.node_pools.node_pool_os
   operating_system_version = var.node_pools.node_pool_os_version
-  shape                    = var.node_pools.node_pool_node_shape
+  shape                    = var.node_pools.node_pool_shape["nodepool${count.index + 1}"]
   sort_by                  = "TIMECREATED"
+  count                    = var.node_pools.node_pools
 }
 
 data "oci_containerengine_cluster_option" "k8s_cluster_option" {
