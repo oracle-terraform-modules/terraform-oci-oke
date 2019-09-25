1428934ff758928f1b8a301b7cff6f6a40196db0
Ali Mukadam <ali.mukadam@oracle.com>
2019-09-25 12:21:30 +1000

    Changed required Terraform version to 0.12.8

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 terraform {
-  required_version = ">= 0.12.5"
+  required_version = ">= 0.12.8"
 }
 
 module "base" {
