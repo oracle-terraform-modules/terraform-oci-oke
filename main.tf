7befc11369fcaf4beb9907835cc0e0885d95f6d0
Ali Mukadam <ali.mukadam@oracle.com>
2020-01-21 21:17:18 +1100

    updated base module to use Autonomous platform image instead of marke… (#118)
    * updated base module to use Autonomous platform image instead of marketplace. Also disabled kubernetes dashboard by default (#116)
    
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>
    
    * updated instructions
    
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>
    
    * base module pointing directly to terraform-oci-base, local copy removed, kubernetes dashboard disabled by default, changelog updated
    
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>
    
    * minor doc correction
    
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
 }
 
 module "base" {
-  source = "./modules/base"
+  source = "github.com/oracle-terraform-modules/terraform-oci-base?ref=v1.1.0"
 
   # identity
   oci_base_identity = local.oci_base_identity
