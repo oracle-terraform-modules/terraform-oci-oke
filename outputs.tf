c8dc847a24163d228daa00fba0138292b22dffae
Ali Mukadam <ali.mukadam@oracle.com>
2020-04-06 10:33:04 +1000

    added network outputs to facilitate reuse from hashicorp registry (#147)
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,30 @@
 # Copyright 2017, 2019, Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
+# for reuse 
+
+output "ig_route_id" {
+  description = "id of route table to vcn internet gateway"
+  value       = module.base.ig_route_id
+}
+
+output "nat_route_id" {
+  description = "id of route table to nat gateway attached to vcn"
+  value       = module.base.nat_route_id
+}
+
+output "subnet_ids" {
+  description = "map of subnet ids (worker, int_lb, pub_lb) used by OKE."
+  value = module.network.subnet_ids
+}
+
+output "vcn_id" {
+  description = "id of vcn where oke is created. use this vcn id to add additional resources"
+  value       = module.base.vcn_id
+}
+
+# convenient output
+
 output "bastion_public_ip" {
   description = "public ip address of bastion host"
   value       = module.base.bastion_public_ip
