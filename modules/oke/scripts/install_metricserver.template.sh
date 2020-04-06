861fdddbaff75f3fd4e45d8de10065686b179ad0
Ali Mukadam <ali.mukadam@oracle.com>
2020-04-06 10:59:16 +1000

    updated metrics_server repo url and dependent folder (#148)
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>

--- modules/oke/scripts/install_metricserver.template.sh
+++ modules/oke/scripts/install_metricserver.template.sh
@@ -2,9 +2,9 @@
 # Copyright 2017, 2019, Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
-git clone https://github.com/kubernetes-incubator/metrics-server.git /tmp/metricserver
+git clone https://github.com/kubernetes-sigs/metrics-server.git /tmp/metricserver
 cd /tmp/metricserver
-kubectl create -f deploy/1.8+/
+kubectl create -f deploy/kubernetes/
 
 sleep 5
 rm -rf /tmp/metricserver/
