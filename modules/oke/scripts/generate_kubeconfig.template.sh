49a4227054642ff4a0a449579c5b82cf5db4100a
Ali Mukadam <ali.mukadam@oracle.com>
2020-04-07 14:22:48 +1000

    upgraded base module to 1.1.3, added wait until admin and instance_principal is ready to generate kubeconfig
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>

--- modules/oke/scripts/generate_kubeconfig.template.sh
+++ modules/oke/scripts/generate_kubeconfig.template.sh
@@ -2,8 +2,10 @@
 # Copyright 2017, 2019, Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
-while [ ! -f /home/opc/admin.finish ]
+
+while [ ! -f /home/opc/admin.finish ]  || [ ! -f /home/opc/ip.finish  ];
 do
-  sleep 30
+  echo "waiting for admin to be ready"; sleep 10;
 done
+
 oci ce cluster create-kubeconfig --cluster-id ${cluster-id} --file $HOME/.kube/config  --region ${region} --token-version 2.0.0
