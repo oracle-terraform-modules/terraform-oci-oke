eee4126029c49b4946b7f67599e5ab48e2648297
Ali Mukadam <ali.mukadam@oracle.com>
2020-04-09 07:31:31 +1000

    added separate delay to ensure OCI_CLI_AUTH is set, kubeconfig is created and instance_principal is active; removed unneceunnecessary token variable version and expiration
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>

--- modules/oke/scripts/generate_kubeconfig.template.sh
+++ modules/oke/scripts/generate_kubeconfig.template.sh
@@ -2,12 +2,4 @@
 # Copyright 2017, 2019, Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
-
-while [ ! -f /home/opc/admin.finish ]  || [ ! -f /home/opc/ip.finish  ];
-do
-  echo "waiting for admin to be ready"; sleep 10;
-done
-
-sleep 30
-
 oci ce cluster create-kubeconfig --cluster-id ${cluster-id} --file $HOME/.kube/config  --region ${region} --token-version 2.0.0
--- modules/oke/variables.tf
+++ modules/oke/variables.tf
@@ -75,14 +75,6 @@ variable "lbs" {
   })
 }
 
-# kubeconfig
-variable "cluster_kube_config_expiration" {
-  default = 2592000
-}
-variable "cluster_kube_config_token_version" {
-  default = "2.0.0"
-}
-
 # ocir
 variable "oke_ocir" {
   type = object({
