12ac32d3606f4a5353edd765e156ce1f90d0c088
Ali Mukadam <ali.mukadam@oracle.com>
2020-06-17 18:25:14 +1000

    Karthicgit master (#174)
    * check node active
    
    Signed-off-by: Karthic Ravindran <karthic.ravindran@oracle.com>
    
    * new line added in variables.tf
    
    Signed-off-by: Karthic Ravindran <karthic.ravindran@oracle.com>
    
    * check_node_active with options
    
    Signed-off-by: Karthic Ravindran <karthic.ravindran@oracle.com>
    
    * changed None to none for uniformity
    
    Signed-off-by: Karthic Ravindran <karthic.ravindran@oracle.com>
    
    * doc changes
    
    Signed-off-by: Karthic Ravindran <karthic.ravindran@oracle.com>
    
    * using kubectl to check active worker nodes
    
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>
    
    Co-authored-by: Karthic Ravindran <karthic.ravindran@oracle.com>

--- modules/oke/activeworker.tf
+++ modules/oke/activeworker.tf
@@ -1,43 +1,43 @@
-# # Copyright 2017, 2019, Oracle Corporation and/or affiliates.  All rights reserved.
-# # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
+# Copyright 2017, 2019, Oracle Corporation and/or affiliates.  All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
-# data "template_file" "check_worker_node_status" {
-#   template = file("${path.module}/scripts/is_worker_active.py")
+data "template_file" "check_active_worker" {
+  template = file("${path.module}/scripts/check_worker_active.template.sh")
 
-#   vars = {
-#     cluster_id     = oci_containerengine_cluster.k8s_cluster.id
-#     compartment_id = var.oke_identity.compartment_id
-#     region         = var.oke_general.region
-#   }
-#   count = var.oke_admin.admin_enabled == true ? 1 : 0
-# }
+  vars = {
+    check_node_active = var.check_node_active
+    total_nodes       = local.total_nodes
+  }
+  count = var.oke_operator.operator_enabled == true && var.check_node_active != "none" ? 1 : 0
+}
 
-# resource null_resource "is_worker_active" {
-#   connection {
-#     host        = var.oke_admin.admin_private_ip
-#     private_key = file(var.oke_ssh_keys.ssh_private_key_path)
-#     timeout     = "40m"
-#     type        = "ssh"
-#     user        = "opc"
+resource null_resource "check_worker_active" {
+  connection {
+    host        = var.oke_operator.operator_private_ip
+    private_key = file(var.oke_ssh_keys.ssh_private_key_path)
+    timeout     = "40m"
+    type        = "ssh"
+    user        = "opc"
 
-#     bastion_host        = var.oke_admin.bastion_public_ip
-#     bastion_user        = "opc"
-#     bastion_private_key = file(var.oke_ssh_keys.ssh_private_key_path)
-#   }
+    bastion_host        = var.oke_operator.bastion_public_ip
+    bastion_user        = "opc"
+    bastion_private_key = file(var.oke_ssh_keys.ssh_private_key_path)
+  }
 
-#   depends_on = [oci_containerengine_node_pool.nodepools]
+  depends_on = [null_resource.write_kubeconfig_on_operator]
 
-#   provisioner "file" {
-#     content     = data.template_file.check_worker_node_status[0].rendered
-#     destination = "~/is_worker_active.py"
-#   }
+  provisioner "file" {
+    content     = data.template_file.check_active_worker[0].rendered
+    destination = "~/check_active_worker.sh"
+  }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x $HOME/is_worker_active.py",
-#       "while [ ! -f $HOME/node.active ]; do $HOME/is_worker_active.py; sleep 10; done",
-#     ]
-#   }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x $HOME/check_active_worker.sh",
+      "$HOME/check_active_worker.sh"
+      //"rm -f $HOME/check_active_worker.sh"
+    ]
+  }
 
-#   count = var.oke_admin.admin_enabled == true ? 1 : 0
-# }
+  count = var.oke_operator.operator_enabled == true && var.check_node_active != "none" ? 1 : 0
+}
