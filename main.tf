85c241ed8a6f387179451a541c2406eb8612e038
Ali Mukadam <ali.mukadam@oracle.com>
2020-10-30 10:29:49 +1100

    updated base module to temporarily disable osms so kubectl and helm can be installed

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
 
 module "base" {
   source  = "oracle-terraform-modules/base/oci"
-  version = "1.3.1"
+  version = "1.3.3"
 
   # general oci parameters
   oci_base_general = local.oci_base_general
