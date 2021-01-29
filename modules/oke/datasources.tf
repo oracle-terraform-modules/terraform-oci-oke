06d22c7473477dd09064ad6b70ea44ff866c96c0
Ali Mukadam <ali.mukadam@oracle.com>
2021-01-29 16:36:40 +1100

    changed the way image for node pools is looked up

--- modules/oke/datasources.tf
+++ modules/oke/datasources.tf
@@ -1,17 +1,12 @@
 # Copyright 2017, 2019, Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
-data "oci_core_images" "latest_images" {
-  for_each                 = var.node_pools.node_pools
-  compartment_id           = var.compartment_id
-  operating_system         = var.node_pools.node_pool_os
-  operating_system_version = var.node_pools.node_pool_os_version
-  shape                    = lookup(each.value,"shape","VM.Standard.E3.Flex")
-  sort_by                  = "TIMECREATED"
-}
-
 data "oci_containerengine_node_pools" "all_node_pools" {
   compartment_id = var.compartment_id
   cluster_id     = oci_containerengine_cluster.k8s_cluster.id
   depends_on     = [oci_containerengine_node_pool.nodepools]
+}
+
+data "oci_containerengine_node_pool_option" "node_pool_options" {
+  node_pool_option_id = oci_containerengine_cluster.k8s_cluster.id
 }
