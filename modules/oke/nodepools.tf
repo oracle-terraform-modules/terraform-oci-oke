ae1b235c5f60050c26b8534f7ad89ef37ea5c703
Ali Mukadam <ali.mukadam@oracle.com>
2021-02-16 17:20:33 +1100

    added ignore node pool image id to lifecycle ignore change so the node pool is not destroyed

--- modules/oke/nodepools.tf
+++ modules/oke/nodepools.tf
@@ -41,8 +41,9 @@ resource "oci_containerengine_node_pool" "nodepools" {
 
   ssh_public_key = file(var.oke_ssh_keys.ssh_public_key_path)
 
+  # do not destroy the node pool if the kubernetes version has changed as part of the upgrade or the node pool image id has been updated
   lifecycle {
-    ignore_changes = [kubernetes_version]
+    ignore_changes = [kubernetes_version, node_source_details.image_id]
   }
   # initial_node_labels {
   #   key   = "key"
