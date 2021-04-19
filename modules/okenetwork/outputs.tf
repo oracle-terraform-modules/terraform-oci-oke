63620546b906b87fe7cabf0006fb2b71ecf95ba9
Ali Mukadam <ali.mukadam@oracle.com>
2021-04-19 20:55:49 +1000

    Bug fix for deprecated map function (#285)
    * removed deprecated map function and use tomap instead
    
    * fixed map syntax since it was already a map
    
    Co-authored-by: Karthic Ravindran <karthic.ravindran@oracle.com>

--- modules/okenetwork/outputs.tf
+++ modules/okenetwork/outputs.tf
@@ -2,10 +2,10 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 output "subnet_ids" {
-  value = map(
-    "cp", join(",", oci_core_subnet.cp[*].id),
-    "workers", join(",", oci_core_subnet.workers[*].id),
-    "int_lb", join(",", oci_core_subnet.int_lb[*].id),
-    "pub_lb", join(",", oci_core_subnet.pub_lb[*].id)
-  )
+  value = {
+      "cp"      = join(",", oci_core_subnet.cp[*].id)
+      "workers" = join(",", oci_core_subnet.workers[*].id)
+      "int_lb"  = join(",", oci_core_subnet.int_lb[*].id)
+      "pub_lb"  = join(",", oci_core_subnet.pub_lb[*].id)
+    }  
 }
