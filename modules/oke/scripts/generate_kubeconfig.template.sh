baa8493323c1d3e21e716d31ba70711ea959570d
Ali Mukadam <ali.mukadam@oracle.com>
2021-09-06 16:02:27 +1000

    feat: always use PRIVATE_ENDPOINT from operator host (#361)
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>

--- modules/oke/scripts/generate_kubeconfig.template.sh
+++ modules/oke/scripts/generate_kubeconfig.template.sh
@@ -3,7 +3,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 if [ ! -f $HOME/.kube/config ]; then
-  oci ce cluster create-kubeconfig --cluster-id ${cluster-id} --file $HOME/.kube/config  --region ${region} --token-version 2.0.0 --auth instance_principal
+  oci ce cluster create-kubeconfig --cluster-id ${cluster-id} --file $HOME/.kube/config  --region ${region} --token-version 2.0.0 --auth instance_principal --kube-endpoint PRIVATE_ENDPOINT
 
   chmod go-r $HOME/.kube/config
 fi
