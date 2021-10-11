f73b1afca796385665858ba1d27253e52e906dec
Karthic Ravindran <karthic.ravindran@oracle.com>
2021-10-11 16:37:11 +0530

    adding seclist for controlplane endpoint (#410)

--- modules/bastionsvc/bastion.tf
+++ modules/bastionsvc/bastion.tf
@@ -2,7 +2,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 resource "oci_bastion_bastion" "bastion" {
-  bastion_type                 = "standard"
+  bastion_type                 = "STANDARD"
   compartment_id               = var.compartment_id
   target_subnet_id             = data.oci_core_subnets.bastion_svc_target_subnet.subnets[0].id
   client_cidr_block_allow_list = var.bastion_service_access
