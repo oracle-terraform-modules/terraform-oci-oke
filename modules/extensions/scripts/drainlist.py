251ca7cc8cf8700f45e0d64f2e1ad0cb2adeeff8
Ali Mukadam <ali.mukadam@oracle.com>
2021-10-25 12:14:12 +1100

    Updated copyright headers, docs formatting and instructions.
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>

--- modules/extensions/scripts/drainlist.py
+++ modules/extensions/scripts/drainlist.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Copyright 2017, 2020, Oracle Corporation and/or affiliates.  All rights reserved.
+# Copyright 2017, 2020, Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 # Derived and adapted from https://www.ateam-oracle.com/secure-way-of-managing-secrets-in-oci
 
--- scripts/dashboard.sh
+++ scripts/dashboard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#  Copyright 2017, 2021 Oracle Corporation and/or affiliates.  All rights reserved.
+#  Copyright 2017, 2021 Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 export KUBECONFIG=generated/kubeconfig
