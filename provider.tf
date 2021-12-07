0aa01134fd0abfdcb7b3f58b4fc19f6e98fb8e86
Michel <mnemonic01@users.noreply.github.com>
2021-12-07 15:29:55 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,7 +10,7 @@ provider "oci" {
   fingerprint      = var.api_fingerprint
   private_key_path = var.api_private_key_path
   region           = var.home_region
-  tenancy_id     = var.tenancy_ocid
+  tenancy_id     = var.tenancy_id
   user_id        = var.user_id
   alias            = "home"
 }
