c87b185deb1fc6ea96daf5d1b97c915f3faa8055
Michel <mnemonic01@users.noreply.github.com>
2021-12-10 21:56:41 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
-#provider "oci" {
-#fingerprint      = var.api_fingerprint
- # private_key_path = var.api_private_key_path
-#  region           = var.region
- # tenancy_ocid     = var.tenancy_id
-#  user_ocid        = var.user_id
-#}
+provider "oci" {
+fingerprint      = var.api_fingerprint
+private_key_path = var.api_private_key_path
+region           = var.region
+tenancy_ocid     = var.tenancy_id
+user_ocid        = var.user_id
+}
 
 provider "oci" {
   fingerprint      = var.api_fingerprint
