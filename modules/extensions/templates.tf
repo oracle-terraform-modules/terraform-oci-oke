a01dca577b50cd9d953879cdc568b79111e51d0b
Ali Mukadam <ali.mukadam@oracle.com>
2022-03-11 22:48:21 +1100

    feat: Added fss storage module.  (#491)
    * feat: Add FSS provisioning
    
    * FSS Storage module added
    
    * enable_fss changed to create_fss
    
    * changed enable_fss to create_fss in main.tf
    
    * added worker nsg for fss
    
    * FSS nsg rules moved to network module
    
    * formatting and removing create_fss variable
    
    Co-authored-by: Karthic Ravindran <karthic.ravindran@oracle.com>

--- modules/extensions/templates.tf
+++ modules/extensions/templates.tf
@@ -100,7 +100,7 @@ locals {
 
   update_dynamic_group_template = templatefile("${path.module}/scripts/update_dynamic_group.template.sh",
     {
-      dynamic_group_id   = var.use_encryption == true ? var.cluster_kms_dynamic_group_id : "null"
+      dynamic_group_id   = var.use_cluster_encryption == true ? var.cluster_kms_dynamic_group_id : "null"
       dynamic_group_rule = local.dynamic_group_rule_this_cluster
       home_region        = data.oci_identity_regions.home_region.regions[0].name
     }
---
+++ modules/storage/fss.tf
@@ -0,0 +1,52 @@
+# Copyright 2017, 2021 Oracle Corporation and/or affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
+
+terraform {
+  required_providers {
+    oci = {
+      source = "hashicorp/oci"
+      # pass oci home region provider explicitly for identity operations
+      configuration_aliases = [oci.home]
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+# Create file system
+resource "oci_file_storage_file_system" "fss" {
+  availability_domain = local.availability_domain
+  compartment_id      = var.compartment_id
+  display_name        = var.label_prefix == "none" ? "fss" : "${var.label_prefix}-fss"
+  lifecycle {
+    ignore_changes = [availability_domain, defined_tags]
+  }
+}
+
+# create file system mount target
+resource "oci_file_storage_mount_target" "fss_mount_target" {
+  availability_domain = local.availability_domain
+  compartment_id      = var.compartment_id
+  subnet_id           = oci_core_subnet.fss.id
+  display_name        = var.label_prefix == "none" ? "fss-mt" : "${var.label_prefix}-fss-mt"
+  hostname_label      = "fss-mt"
+  nsg_ids             = [oci_core_network_security_group.fss_mt.id]
+
+  lifecycle {
+    ignore_changes = [availability_domain, defined_tags]
+  }
+}
+
+# FSS export set associate with mount target
+resource "oci_file_storage_export_set" "export_sets" {
+  mount_target_id   = oci_file_storage_mount_target.fss_mount_target.id
+  display_name      = var.label_prefix == "none" ? "fss-storage-export" : "${var.label_prefix}-fss-storage-export"
+  max_fs_stat_bytes = var.max_fs_stat_bytes
+  max_fs_stat_files = var.max_fs_stat_files
+}
+
+# FSS file storage export
+resource "oci_file_storage_export" "exports" {
+  export_set_id  = oci_file_storage_export_set.export_sets.id
+  file_system_id = oci_file_storage_file_system.fss.id
+  path           = var.fss_mount_path
+}
