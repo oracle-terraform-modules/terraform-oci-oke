f622d515c771bec3c71330dde36b528bcac73d20
Karthic Ravindran <karthic.ravindran@oracle.com>
2022-03-22 13:34:07 +0530

    removed commented lines

--- modules/extensions/k8stools.tf
+++ modules/extensions/k8stools.tf
@@ -36,35 +36,3 @@ resource "null_resource" "install_kubectl_on_operator" {
 
   count = var.create_bastion_host == true && var.bastion_state == "RUNNING" && var.create_operator == true && var.operator_state == "RUNNING" ? 1 : 0
 }
-
-# helm
-# resource "null_resource" "install_helm_on_operator" {
-#   connection {
-#     host        = var.operator_private_ip
-#     private_key = local.ssh_private_key
-#     timeout     = "40m"
-#     type        = "ssh"
-#     user        = "opc"
-
-#     bastion_host        = var.bastion_public_ip
-#     bastion_user        = "opc"
-#     bastion_private_key = local.ssh_private_key
-#   }
-
-#   depends_on = [null_resource.install_kubectl_on_operator, null_resource.write_kubeconfig_on_operator]
-
-#   provisioner "file" {
-#     content     = local.install_helm_template
-#     destination = "/home/opc/install_helm.sh"
-#   }
-
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x $HOME/install_helm.sh",
-#       "bash $HOME/install_helm.sh",
-#       "rm -f $HOME/install_helm.sh"
-#     ]
-#   }
-
-#   count = var.create_bastion_host == true && var.bastion_state == "RUNNING" && var.create_operator == true ? 1 : 0
-# }
