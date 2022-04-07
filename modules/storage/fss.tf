02bb57f92a93465a1992d5e87d1726adde4e94e3
Ali Mukadam <ali.mukadam@oracle.com>
2022-04-07 16:49:52 +1000

    fix: specified oci provider namespace in submodule network and      bastionsvc
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>

--- modules/storage/fss.tf
+++ modules/storage/fss.tf
@@ -1,18 +1,6 @@
 # Copyright 2017, 2021 Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
-terraform {
-  required_providers {
-    oci = {
-      source = "oracle/oci"
-      # pass oci home region provider explicitly for identity operations
-      configuration_aliases = [oci.home]
-      version               = ">= 4.67.3"
-    }
-  }
-  required_version = ">= 1.0.0"
-}
-
 # Create file system
 resource "oci_file_storage_file_system" "fss" {
   availability_domain = local.availability_domain
---
+++ modules/storage/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+      # pass oci home region provider explicitly for identity operations
+      configuration_aliases = [oci.home]
+      version               = ">= 4.67.3"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
