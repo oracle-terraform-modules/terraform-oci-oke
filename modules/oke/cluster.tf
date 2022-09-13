ee4e0753912ab653761a3c0c3c6e506c75f7e269
Ali Mukadam <ali.mukadam@oracle.com>
2022-09-13 10:59:13 +1000

    fix: changed lookup mechanism for tags for cluster and node pools
    Resolves #573
    
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>

--- modules/oke/cluster.tf
+++ modules/oke/cluster.tf
@@ -23,7 +23,7 @@ resource "oci_containerengine_cluster" "k8s_cluster" {
   endpoint_config {
     is_public_ip_enabled = var.control_plane_type == "public" ? true : false
     nsg_ids              = var.control_plane_nsgs
-    subnet_id            = var.cluster_subnets["cp"]
+    subnet_id            = lookup(var.cluster_subnets,"cp")
   }
 
   dynamic "image_policy_config" {
@@ -43,8 +43,8 @@ resource "oci_containerengine_cluster" "k8s_cluster" {
     }
   }
 
-  freeform_tags = var.freeform_tags["cluster"]
-  defined_tags  = var.defined_tags["cluster"]
+  freeform_tags = lookup(var.freeform_tags,"cluster",{})
+  defined_tags  = lookup(var.defined_tags,"cluster",{})
 
   options {
     add_ons {
@@ -53,7 +53,7 @@ resource "oci_containerengine_cluster" "k8s_cluster" {
     }
 
     admission_controller_options {
-      is_pod_security_policy_enabled = var.admission_controller_options["PodSecurityPolicy"]
+      is_pod_security_policy_enabled = lookup(var.admission_controller_options,"PodSecurityPolicy",false)
     }
 
     kubernetes_network_config {
@@ -62,13 +62,13 @@ resource "oci_containerengine_cluster" "k8s_cluster" {
     }
 
     persistent_volume_config {
-      freeform_tags = var.freeform_tags["persistent_volume"]
-      defined_tags  = var.defined_tags["persistent_volume"]
+      freeform_tags = lookup(var.freeform_tags,"persistent_volume",{})
+      defined_tags  = lookup(var.defined_tags,"persistent_volume",{})
     }
 
     service_lb_config {
-      freeform_tags = var.freeform_tags["service_lb"]
-      defined_tags  = var.defined_tags["service_lb"]
+      freeform_tags = lookup(var.freeform_tags,"service_lb",{})
+      defined_tags  = lookup(var.defined_tags,"service_lb",{})
     }
 
     service_lb_subnet_ids = var.preferred_load_balancer == "public" ? [var.cluster_subnets["pub_lb"]] : [var.cluster_subnets["int_lb"]]
