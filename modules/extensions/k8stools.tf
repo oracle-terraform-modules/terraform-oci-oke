d931c16e5d3d7f3b8bfafc3fd7b65bd7382d24e7
Devon Crouse <devon.crouse@oracle.com>
2022-11-25 09:17:21 -0700

    Wait for cloud-init completion before k8s extension install
    Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

--- modules/extensions/k8stools.tf
+++ modules/extensions/k8stools.tf
@@ -1,4 +1,4 @@
-# Copyright 2017, 2021 Oracle Corporation and/or affiliates.
+# Copyright 2017, 2022 Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 resource "null_resource" "install_k8stools_on_operator" {
@@ -21,6 +21,7 @@ resource "null_resource" "install_k8stools_on_operator" {
 
   provisioner "remote-exec" {
     inline = [
+      "cloud-init status --wait",
       "if [ -f \"$HOME/install_kubectx.sh\" ]; then bash \"$HOME/install_kubectx.sh\"; rm -f \"$HOME/install_kubectx.sh\";fi",
     ]
   }
