95dbbc274bbeb564b63169315eea1ca9ea9e0ff7
Devon Crouse <devon.crouse@oracle.com>
2022-12-02 22:42:52 -0700

    fix: Remove label_prefix from dns_label in subnets (#620)
    Signed-off-by: Devon Crouse <devon.crouse@oracle.com>
    
    Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

--- modules/network/subnets.tf
+++ modules/network/subnets.tf
@@ -1,11 +1,11 @@
-# Copyright 2017, 2021 Oracle Corporation and/or affiliates.
+# Copyright 2017, 2022 Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 resource "oci_core_subnet" "cp" {
   cidr_block                 = local.cp_subnet
   compartment_id             = var.compartment_id
   display_name               = var.label_prefix == "none" ? "control-plane" : "${var.label_prefix}-control-plane"
-  dns_label                  = var.assign_dns ? var.label_prefix == "none" ? "cp" : "${var.label_prefix}cp" : null
+  dns_label                  = var.assign_dns ? "cp" : null
   prohibit_public_ip_on_vnic = var.control_plane_type == "private" ? true : false
   route_table_id             = var.control_plane_type == "private" ? var.nat_route_id : var.ig_route_id
   security_list_ids          = [oci_core_security_list.control_plane_seclist.id]
@@ -20,7 +20,7 @@ resource "oci_core_subnet" "workers" {
   cidr_block                 = local.workers_subnet
   compartment_id             = var.compartment_id
   display_name               = var.label_prefix == "none" ? "workers" : "${var.label_prefix}-workers"
-  dns_label                  = var.assign_dns ? var.label_prefix == "none" ? "workers" : "${var.label_prefix}wo" : null
+  dns_label                  = var.assign_dns ? "wo" : null
   prohibit_public_ip_on_vnic = var.worker_type == "private" ? true : false
   route_table_id             = var.worker_type == "private" ? var.nat_route_id : var.ig_route_id
   vcn_id                     = var.vcn_id
@@ -34,7 +34,7 @@ resource "oci_core_subnet" "pods" {
   cidr_block                 = local.pods_subnet
   compartment_id             = var.compartment_id
   display_name               = var.label_prefix == "none" ? "pods" : "${var.label_prefix}-pods"
-  dns_label                  = var.assign_dns ? var.label_prefix == "none" ? "pods" : "${var.label_prefix}po" : null
+  dns_label                  = var.assign_dns ? "po" : null
   prohibit_public_ip_on_vnic = true
   route_table_id             = var.nat_route_id
   vcn_id                     = var.vcn_id
@@ -50,7 +50,7 @@ resource "oci_core_subnet" "int_lb" {
   cidr_block                 = local.int_lb_subnet
   compartment_id             = var.compartment_id
   display_name               = var.label_prefix == "none" ? "int-lb" : "${var.label_prefix}-int-lb"
-  dns_label                  = var.assign_dns ? var.label_prefix == "none" ? "intlb" : "${var.label_prefix}ilb" : null
+  dns_label                  = var.assign_dns ? "ilb" : null
   prohibit_public_ip_on_vnic = true
   route_table_id             = var.nat_route_id
   vcn_id                     = var.vcn_id
@@ -66,7 +66,7 @@ resource "oci_core_subnet" "pub_lb" {
   cidr_block                 = local.pub_lb_subnet
   compartment_id             = var.compartment_id
   display_name               = var.label_prefix == "none" ? "pub-lb" : "${var.label_prefix}-pub-lb"
-  dns_label                  = var.assign_dns ? var.label_prefix == "none" ? "publb" : "${var.label_prefix}plb" : null
+  dns_label                  = var.assign_dns ? "plb" : null
   prohibit_public_ip_on_vnic = false
   route_table_id             = var.ig_route_id
   vcn_id                     = var.vcn_id
