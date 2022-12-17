oracle-terraform-modules/terraform-oci-oke

commit 269d3fdd896309157e667558bb885f0c54a3e11d

fix: Ignore lifecycle changes, support Resource Manager variables (#583)
* Remove extraneous icmp_options reporting change each apply

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

* Ignore node pool changes for automatic tagging and dynamic variables reporting change each apply

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

* Use specific compartment for AD listing

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

* Ignore cluster changes for automatic tagging reporting change each apply

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

* Suppress reported output changes on each apply/destroy

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

* Ignore dynamic kubeconfig changes reported each apply w/ refresh option

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

* Support variable naming populated by Resource Manager

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

* Provided bastion IP fallback if creation disabled, omit -i if no key file for output var

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

* Use stable random suffix for dynamic group name

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

* Fall back to provided bastion/operator addresses, user config

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

* Clarify TF resource naming for operator IAM

* Update modules: bastion 3.1.2, operator 3.1.1

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

modules/oke/nodepools.tf
@@ -118,7 +118,13 @@ resource "oci_containerengine_node_pool" "nodepools" {
   # do not destroy the node pool if the kubernetes version has changed as part of the upgrade
 
   lifecycle {
-    ignore_changes = [kubernetes_version]
+    ignore_changes = [
+      kubernetes_version,
+      defined_tags, # automatic tagging after apply
+      node_metadata["user_data"], # templated cloud-init
+      node_config_details["placement_configs"], # dynamic placement configs
+      node_source_details # dynamic image lookup
+    ]
   }
 
   # initial node labels for the different node pools

datasource.tf
@@ -4,7 +4,7 @@
 data "oci_core_vcns" "vcns" {
   count = var.create_vcn == true ? 0 : 1
 
-  compartment_id = var.compartment_id
+  compartment_id = local.compartment_id
   display_name   = var.vcn_display_name
 
   state = "AVAILABLE"
@@ -13,7 +13,7 @@ data "oci_core_vcns" "vcns" {
 data "oci_core_route_tables" "nat" {
   count = var.create_vcn == true ? 0 : 1
 
-  compartment_id = var.compartment_id
+  compartment_id = local.compartment_id
 
   display_name = var.nat_route_table_display_name
   vcn_id       = local.vcn_id
@@ -24,7 +24,7 @@ data "oci_core_route_tables" "nat" {
 data "oci_core_route_tables" "ig" {
   count = var.create_vcn == true ? 0 : 1
 
-  compartment_id = var.compartment_id
+  compartment_id = local.compartment_id
 
   display_name = var.ig_route_table_display_name
   vcn_id       = local.vcn_id

locals.tf
@@ -2,13 +2,20 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 locals {
-  bastion_public_ip                      = var.create_bastion_host == true ? module.bastion[0].bastion_public_ip : ""
-  operator_private_ip                    = var.create_operator == true ? module.operator[0].operator_private_ip : ""
+  tenancy_id     = coalesce(var.tenancy_id, var.tenancy_ocid)
+  compartment_id = coalesce(
+    var.compartment_id, var.compartment_ocid,
+    var.tenancy_id, var.tenancy_ocid,
+  )
+
+  bastion_public_ip                      = var.create_bastion_host == true ? module.bastion[0].bastion_public_ip : coalesce(var.bastion_public_ip, "")
+  operator_private_ip                    = var.create_operator == true ? module.operator[0].operator_private_ip : coalesce(var.operator_private_ip, "")
   operator_instance_principal_group_name = var.create_operator == true ? module.operator[0].operator_instance_principal_group_name : ""
 
   vcn_id       = var.create_vcn == true ? module.vcn[0].vcn_id : coalesce(var.vcn_id, try(data.oci_core_vcns.vcns[0].virtual_networks[0].id,""))
   ig_route_id  = var.create_vcn == true ? module.vcn[0].ig_route_id : coalesce(var.ig_route_table_id, try(data.oci_core_route_tables.ig[0].route_tables[0].id,""))
   nat_route_id = var.create_vcn == true ? module.vcn[0].nat_route_id : coalesce(var.nat_route_table_id, try(data.oci_core_route_tables.nat[0].route_tables[0].id,""))
 
+  ssh_key_arg                            = var.ssh_private_key_path == "none" ? "" : " -i ${var.ssh_private_key_path}"
   validate_drg_input = var.create_drg && (var.drg_id != null) ? tobool("[ERROR]: create_drg variable can not be true if drg_id is provided.]") : true
 }

main.tf
@@ -6,7 +6,7 @@ module "vcn" {
   version = "3.5.1"
 
   # general oci parameters
-  compartment_id = var.compartment_id
+  compartment_id = local.compartment_id
   label_prefix   = var.label_prefix
 
   # gateways
@@ -35,12 +35,11 @@ module "vcn" {
 }
 
 module "drg" {
-
   source  = "oracle-terraform-modules/drg/oci"
   version = "1.0.3"
 
   # general oci parameters
-  compartment_id = var.compartment_id
+  compartment_id = local.compartment_id
   label_prefix   = var.label_prefix
 
   # drg parameters
@@ -60,10 +59,10 @@ module "drg" {
 
 module "bastion" {
   source  = "oracle-terraform-modules/bastion/oci"
-  version = "3.1.1"
+  version = "3.1.2"
 
-  tenancy_id     = var.tenancy_id
-  compartment_id = var.compartment_id
+  tenancy_id     = local.tenancy_id
+  compartment_id = local.compartment_id
 
   label_prefix = var.label_prefix
 
@@ -109,12 +108,12 @@ module "bastion" {
 
 module "operator" {
   source  = "oracle-terraform-modules/operator/oci"
-  version = "3.1.0"
+  version = "3.1.1"
 
-  tenancy_id = var.tenancy_id
 
   # general oci parameters
-  compartment_id = var.compartment_id
+  tenancy_id     = local.tenancy_id
+  compartment_id = local.compartment_id
   label_prefix   = var.label_prefix
 
   # networking
@@ -162,7 +161,7 @@ module "bastionsvc" {
   source = "./modules/bastionsvc"
 
   # general oci parameters
-  compartment_id = var.compartment_id
+  compartment_id = local.compartment_id
   label_prefix   = var.label_prefix
 
   # bastion service parameters
@@ -184,7 +183,7 @@ module "network" {
   source = "./modules/network"
 
   # general oci parameters
-  compartment_id = var.compartment_id
+  compartment_id = local.compartment_id
   label_prefix   = var.label_prefix
 
   # oke networking parameters
@@ -233,10 +232,10 @@ module "oke" {
   source = "./modules/oke"
 
   # provider
-  tenancy_id = var.tenancy_id
+  tenancy_id = local.tenancy_id
 
   # general oci parameters
-  compartment_id = var.compartment_id
+  compartment_id = local.compartment_id
   label_prefix   = var.label_prefix
 
   # ssh keys
@@ -301,8 +300,8 @@ module "storage" {
   source = "./modules/storage"
 
   # general oci parameters
-  tenancy_id          = var.tenancy_id
-  compartment_id      = var.compartment_id
+  tenancy_id          = local.tenancy_id
+  compartment_id      = local.compartment_id
   availability_domain = var.availability_domains["fss"]
   label_prefix        = var.label_prefix
 
@@ -330,10 +329,10 @@ module "extensions" {
   source = "./modules/extensions"
 
   # provider
-  tenancy_id = var.tenancy_id
+  tenancy_id = local.tenancy_id
 
   # general oci parameters
-  compartment_id = var.compartment_id
+  compartment_id = local.compartment_id
   label_prefix   = var.label_prefix
 
   # region parameters
@@ -348,11 +347,13 @@ module "extensions" {
   # bastion
   create_bastion_host = var.create_bastion_host
   bastion_public_ip   = local.bastion_public_ip
+  bastion_user        = var.bastion_user
   bastion_state       = var.bastion_state
 
   # operator details
   create_operator                    = var.create_operator
   operator_private_ip                = local.operator_private_ip
+  operator_user                      = var.operator_user
   operator_state                     = var.operator_state
   operator_dynamic_group             = local.operator_instance_principal_group_name
   enable_operator_instance_principal = var.enable_operator_instance_principal
@@ -400,7 +401,8 @@ module "extensions" {
   nodepool_upgrade_method = var.nodepool_upgrade_method
   node_pools_to_drain     = var.node_pools_to_drain
 
-  debug_mode = var.debug_mode
+  debug_mode        = var.debug_mode
+  update_kubeconfig = var.update_kubeconfig
 
   depends_on = [
     module.bastion,

modules/extensions/activeworker.tf
@@ -11,10 +11,10 @@ resource "null_resource" "check_worker_active" {
     private_key = local.ssh_private_key
     timeout     = "40m"
     type        = "ssh"
-    user        = "opc"
+    user        = var.operator_user
 
     bastion_host        = var.bastion_public_ip
-    bastion_user        = "opc"
+    bastion_user        = var.bastion_user
     bastion_private_key = local.ssh_private_key
   }
 

modules/extensions/calico.tf
@@ -7,10 +7,10 @@ resource "null_resource" "install_calico" {
     private_key = local.ssh_private_key
     timeout     = "40m"
     type        = "ssh"
-    user        = "opc"
+    user        = var.operator_user
 
     bastion_host        = var.bastion_public_ip
-    bastion_user        = "opc"
+    bastion_user        = var.bastion_user
     bastion_private_key = local.ssh_private_key
   }
 

modules/extensions/drain.tf
@@ -7,10 +7,10 @@ resource "null_resource" "drain_nodes" {
     private_key = local.ssh_private_key
     timeout     = "40m"
     type        = "ssh"
-    user        = "opc"
+    user        = var.operator_user
 
     bastion_host        = var.bastion_public_ip
-    bastion_user        = "opc"
+    bastion_user        = var.bastion_user
     bastion_private_key = local.ssh_private_key
   }
 

modules/extensions/gatekeeper.tf
@@ -7,10 +7,10 @@ resource "null_resource" "enable_gatekeeper" {
     private_key = local.ssh_private_key
     timeout     = "40m"
     type        = "ssh"
-    user        = "opc"
+    user        = var.operator_user
 
     bastion_host        = var.bastion_public_ip
-    bastion_user        = "opc"
+    bastion_user        = var.bastion_user
     bastion_private_key = local.ssh_private_key
   }
 

commit 948ce96ecbed8bf31536016a6ec243b70e3cbb43

Node pool correctly ignore sub-sub-object
Correct terraform syntax to ignore just the needed parts of
`node_metadata` and `node_config_details`

Before the change the whole sub-objects were ignored, leading to
inability to change a nodepool size via terraform.

Fixes: 269d3fdd89

modules/oke/nodepools.tf
@@ -121,8 +121,8 @@ resource "oci_containerengine_node_pool" "nodepools" {
     ignore_changes = [
       kubernetes_version,
       defined_tags, # automatic tagging after apply
-      node_metadata["user_data"], # templated cloud-init
-      node_config_details["placement_configs"], # dynamic placement configs
+      node_metadata, # templated cloud-init
+      node_config_details[0].placement_configs, # dynamic placement configs
       node_source_details # dynamic image lookup
     ]
   }