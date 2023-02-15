fd89e13a2ceb11e447b48e62af436d3a47d80294
Devon Crouse <devon.crouse@oracle.com>
2023-02-14 23:35:39 -0700

    Support additional cloud-init config for worker pools
    Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

--- modules/workers/cloudinit.tf
+++ modules/workers/cloudinit.tf
@@ -1,18 +1,142 @@
 # Copyright (c) 2022, 2023 Oracle Corporation and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
+locals {
+  # https://cloudinit.readthedocs.io/en/latest/explanation/format.html#mime-multi-part-archive
+  default_cloud_init_content_type = "text/x-shellscript"
+
+  # https://canonical-cloud-init.readthedocs-hosted.com/en/latest/reference/merging.html
+  default_cloud_init_merge_type = "list(append)+dict(no_replace,recurse_list)+str(append)"
+}
+
+# https://registry.terraform.io/providers/hashicorp/template/latest/docs/data-sources/cloudinit_config.html
 data "cloudinit_config" "workers" {
   for_each      = local.enabled_worker_pools
-  gzip          = false
+  gzip          = true
   base64_encode = true
 
+  # Include global custom cloud init MIME parts
+  dynamic "part" {
+    for_each = var.cloud_init
+    iterator = global_part
+    content {
+      content      = lookup(global_part.value, "content", "")
+      content_type = lookup(global_part.value, "content_type", local.default_cloud_init_content_type)
+      filename     = lookup(global_part.value, "filename", null)
+      merge_type   = lookup(global_part.value, "merge_type", local.default_cloud_init_merge_type)
+    }
+  }
+
+  # Include pool-specific custom cloud init MIME parts
+  dynamic "part" {
+    for_each = each.value.cloud_init
+    iterator = pool_part
+    content {
+      content      = lookup(pool_part.value, "content", "")
+      content_type = lookup(pool_part.value, "content_type", local.default_cloud_init_content_type)
+      filename     = lookup(pool_part.value, "filename", null)
+      merge_type   = lookup(pool_part.value, "merge_type", local.default_cloud_init_merge_type)
+    }
+  }
+
+  # Set timezone
   part {
-    filename     = "worker.sh"
-    content_type = each.value.os == "Oracle Linux" ? "text/x-shellscript" : "text/x-shellscript-per-boot"
-    content = coalesce(each.value.cloudinit, templatefile("${path.module}/cloudinit/worker.template.sh", {
-      cluster_ca_cert = var.cluster_ca_cert
-      apiserver_host  = var.apiserver_private_host
-      timezone        = var.timezone
-    }))
+    # https://cloudinit.readthedocs.io/en/latest/reference/modules.html#timezone
+    content_type = "text/cloud-config"
+    content      = format("timezone: %s", lookup(each.value, "timezone", var.timezone))
+    filename     = "timezone.yml"
+    merge_type   = local.default_cloud_init_merge_type
+  }
+
+  # Expand root filesystem to fill available space on volume
+  part {
+    content_type = "text/cloud-config"
+    content = jsonencode({
+      # https://cloudinit.readthedocs.io/en/latest/reference/modules.html#growpart
+      growpart = {
+        mode                     = "auto"
+        devices                  = ["/"]
+        ignore_growroot_disabled = false
+      }
+
+      # https://cloudinit.readthedocs.io/en/latest/reference/modules.html#resizefs
+      resize_rootfs = true
+    })
+    filename   = "growpart.yml"
+    merge_type = local.default_cloud_init_merge_type
+  }
+
+  # Write extra OKE configuration to filesystem
+  part {
+    content_type = "text/cloud-config"
+    content = jsonencode({
+      write_files = [
+        {
+          content = var.apiserver_private_host
+          path    = "/etc/oke/oke-apiserver"
+        },
+        {
+          content  = var.cluster_ca_cert
+          encoding = "base64"
+          path     = "/etc/kubernetes/ca.crt"
+        },
+      ]
+    })
+    filename   = "oke-config.yml"
+    merge_type = local.default_cloud_init_merge_type
+  }
+
+  # OKE startup initialization
+  part {
+    content_type = "text/x-shellscript"
+    content      = file("${path.module}/cloudinit-oke.sh")
+    filename     = "oke.sh"
+    merge_type   = local.default_cloud_init_merge_type
+  }
+
+  lifecycle {
+    precondition {
+      condition = alltrue([for c in var.cloud_init :
+        trimspace(lookup(c, "content", "")) != ""
+      ])
+      error_message = <<-EOT
+      Each global cloud_init map entry must include a non-empty 'content' field.
+      See https://registry.terraform.io/providers/hashicorp/template/latest/docs/data-sources/cloudinit_config.html.
+      var.cloud_init (${each.key}): ${try(jsonencode(var.cloud_init), "invalid")}
+      EOT
+    }
+
+    precondition {
+      condition = alltrue([for c in var.cloud_init :
+        length(regexall("^text/[a-z-]*$", trimspace(lookup(c, "content_type", local.default_cloud_init_content_type)))) > 0
+      ])
+      error_message = <<-EOT
+      Each global cloud_init map entry must include a 'content_type' field prefixed with 'text/'.
+      See https://cloudinit.readthedocs.io/en/latest/explanation/format.html#mime-multi-part-archive.
+      var.cloud_init (${each.key}): ${try(jsonencode(var.cloud_init), "invalid")}
+      EOT
+    }
+
+    precondition {
+      condition = alltrue([for c in each.value.cloud_init :
+        trimspace(lookup(c, "content", "")) != ""
+      ])
+      error_message = <<-EOT
+      Each pool-specific cloud_init map entry must include a non-empty 'content' field.
+      See https://registry.terraform.io/providers/hashicorp/template/latest/docs/data-sources/cloudinit_config.html.
+      ${each.key}["cloud_init"]: ${try(jsonencode(each.value.cloud_init), "invalid")}
+      EOT
+    }
+
+    precondition {
+      condition = alltrue([for c in each.value.cloud_init :
+        length(regexall("^text/[a-z-]+$", trimspace(lookup(c, "content_type", local.default_cloud_init_content_type)))) > 0
+      ])
+      error_message = <<-EOT
+      Each pool-specific cloud_init map entry must include a 'content_type' field prefixed with 'text/'.
+      See https://cloudinit.readthedocs.io/en/latest/explanation/format.html#mime-multi-part-archive.
+      ${each.key}["cloud_init"]: ${try(jsonencode(each.value.cloud_init), "invalid")}
+      EOT
+    }
   }
 }
