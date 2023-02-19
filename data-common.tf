b243b8701965cadd4821e0f25a73ccb575794bbf
Devon Crouse <devon.crouse@oracle.com>
2023-02-19 16:38:26 -0700

    Relocate iam definitions to appropriate file
    Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

--- data-common.tf
+++ data-common.tf
@@ -5,26 +5,6 @@ resource "random_id" "state_id" {
   byte_length = 6
 }
 
-data "oci_identity_availability_domains" "all" {
-  compartment_id = local.compartment_id
-}
-
-locals {
-  // Tenancy-specific availability domains in region
-  // Common reference for data source re-used throughout module
-  ads = data.oci_identity_availability_domains.all.availability_domains
-
-  // Map of parsed availability domain numbers to tenancy-specific names
-  // Used by resources with AD placement for generic selection
-  ad_numbers_to_names = local.ads != null ? {
-    for ad in local.ads : parseint(substr(ad.name, -1, -1), 10) => ad.name
-  } : { -1 : "" } # Fallback handles failure when unavailable but not required
-
-  // List of availability domain numbers in region
-  // Used to intersect desired AD lists against presence in region
-  ad_numbers = local.ads != null ? sort(keys(local.ad_numbers_to_names)) : []
-}
-
 output "state_id" {
   value = random_id.state_id.id
 }
