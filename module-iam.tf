7a72dfeceb8e8209828329bd182d90dc1162f58b
Devon Crouse <devon.crouse@oracle.com>
2023-02-19 16:38:26 -0700

    Relocate iam definitions to appropriate file
    Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

--- module-iam.tf
+++ module-iam.tf
@@ -1,7 +1,25 @@
 # Copyright (c) 2022, 2023 Oracle Corporation and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
+data "oci_identity_availability_domains" "all" {
+  compartment_id = local.compartment_id
+}
+
 locals {
+  // Tenancy-specific availability domains in region
+  // Common reference for data source re-used throughout module
+  ads = data.oci_identity_availability_domains.all.availability_domains
+
+  // Map of parsed availability domain numbers to tenancy-specific names
+  // Used by resources with AD placement for generic selection
+  ad_numbers_to_names = local.ads != null ? {
+    for ad in local.ads : parseint(substr(ad.name, -1, -1), 10) => ad.name
+  } : { -1 : "" } # Fallback handles failure when unavailable but not required
+
+  // List of availability domain numbers in region
+  // Used to intersect desired AD lists against presence in region
+  ad_numbers = local.ads != null ? sort(keys(local.ad_numbers_to_names)) : []
+
   create_iam_worker_policy = anytrue([
     var.create_iam_worker_policy == "always",
     var.create_iam_worker_policy == "auto" && var.create_cluster && anytrue([for k, v in var.worker_pools :
