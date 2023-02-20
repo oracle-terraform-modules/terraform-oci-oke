85c775e6244db76b712920212109a34e01ae8fa2
Devon Crouse <devon.crouse@oracle.com>
2023-02-19 18:38:13 -0700

    Update versions.tf, pin to minor versions
    Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

--- versions.tf
+++ versions.tf
@@ -2,33 +2,38 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 terraform {
+  required_version = "~> 1.2.0"
+
   required_providers {
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = ">= 2.2.0"
+      version = "~> 2.2.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.9.0"
     }
 
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.2.1"
+      version = "~> 3.2.1"
     }
 
     oci = {
       configuration_aliases = [oci.home]
       source                = "oracle/oci"
-      version               = ">= 4.67.3"
+      version               = "~> 4.108.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.4.3"
+      version = "~> 3.4.3"
     }
 
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.9.1"
+      version = "~> 0.9.1"
     }
   }
-
-  required_version = ">= 1.1.0"
 }
