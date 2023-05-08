oracle-terraform-modules/terraform-oci-oke

commit 830192e3e59c716f2257c01ff718e9530a92741e

feat: Add oci_containerengine_cluster type with 'basic' or 'enhanced'
Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

versions.tf
@@ -1,4 +1,4 @@
-# Copyright 2017, 2022 Oracle Corporation and/or affiliates.
+# Copyright 2017, 2023 Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 terraform {
@@ -6,8 +6,8 @@ terraform {
     oci = {
       source                = "oracle/oci"
       configuration_aliases = [oci.home]
-      version               = ">= 4.67.3"
+      version               = "~> 4.114.0"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.2.0"
 }

main.tf
@@ -1,4 +1,4 @@
-# Copyright 2017, 2022 Oracle Corporation and/or affiliates.
+# Copyright 2017, 2023 Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 module "vcn" {
@@ -248,10 +248,14 @@ module "oke" {
   ssh_public_key_path = var.ssh_public_key_path
 
   # oke cluster parameters
-  cluster_kubernetes_version                              = var.kubernetes_version
-  control_plane_type                                      = var.control_plane_type
-  control_plane_nsgs                                      = concat(var.control_plane_nsgs, [module.network.control_plane_nsg_id])
-  cluster_name                                            = var.cluster_name
+  cluster_kubernetes_version = var.kubernetes_version
+  control_plane_type         = var.control_plane_type
+  control_plane_nsgs         = concat(var.control_plane_nsgs, [module.network.control_plane_nsg_id])
+  cluster_name               = var.cluster_name
+  cluster_type = lookup({
+    "basic"    = "BASIC_CLUSTER",
+    "enhanced" = "ENHANCED_CLUSTER"
+  }, lower(var.cluster_type), "BASIC_CLUSTER")
   cluster_options_add_ons_is_kubernetes_dashboard_enabled = var.dashboard_enabled
   cluster_options_kubernetes_network_config_pods_cidr     = var.pods_cidr
   cluster_options_kubernetes_network_config_services_cidr = var.services_cidr

modules/oke/cluster.tf
@@ -1,4 +1,4 @@
-# Copyright 2017, 2022 Oracle Corporation and/or affiliates.
+# Copyright 2017, 2023 Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 # 30s delay to allow policies to take effect globally
@@ -13,7 +13,7 @@ resource "oci_containerengine_cluster" "k8s_cluster" {
   kubernetes_version = var.cluster_kubernetes_version
   kms_key_id         = var.use_cluster_encryption == true ? var.cluster_kms_key_id : null
   name               = var.label_prefix == "none" ? var.cluster_name : "${var.label_prefix}-${var.cluster_name}"
-
+  type               = var.cluster_type
   depends_on = [time_sleep.wait_30_seconds]
 
   cluster_pod_network_options {

modules/oke/variables.tf
@@ -1,4 +1,4 @@
-# Copyright (c) 2017, 2022 Oracle Corporation and/or its affiliates.
+# Copyright (c) 2017, 2023 Oracle Corporation and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 # oci provider
@@ -25,6 +25,7 @@ variable "control_plane_nsgs" {
 }
 
 variable "cluster_name" {}
+variable "cluster_type" {}
 
 variable "cluster_options_add_ons_is_kubernetes_dashboard_enabled" {
   type = bool

terraform.tfvars.example
@@ -168,6 +168,7 @@ allow_pod_internet_access    = true
 allow_worker_internet_access = true
 allow_worker_ssh_access      = false
 cluster_name                 = "oke"
+cluster_type                 = "basic"
 cni_type                     = "flannel"
 control_plane_type           = "private"
 control_plane_allowed_cidrs  = ["0.0.0.0/0"]

variables.tf
@@ -1,4 +1,4 @@
-# Copyright (c) 2017, 2022 Oracle Corporation and/or its affiliates.
+# Copyright (c) 2017, 2023 Oracle Corporation and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 # OCI Provider parameters
@@ -543,6 +543,16 @@ variable "cluster_name" {
   default     = "oke"
   description = "The name of oke cluster."
   type        = string
+}
+
+variable "cluster_type" {
+  default     = "basic"
+  description = "The cluster type. See <a href=https://docs.oracle.com/en-us/iaas/Content/ContEng/Tasks/contengworkingwithenhancedclusters.htm>Working with Enhanced Clusters and Basic Clusters</a> for more information."
+  type        = string
+  validation {
+    condition     = contains(["basic", "enhanced"], lower(var.cluster_type))
+    error_message = "Accepted values are 'basic' or 'enhanced'."
+  }
 }
 
 variable "cni_type" {

commit 5061ff1973c7727c8f1bead36cb55c0bbc2b8ae5

Use lower bound version specification for oci provider
The oke module is a reusable module, hence it must not
force the whole root module to un under a certain minor
release of the oci provider.

Fixes: 830192e3e59c716f
Signed-off-by: Andrea Stacchiotti <andreastacchiotti@gmail.com>

versions.tf
@@ -6,7 +6,7 @@ terraform {
     oci = {
       source                = "oracle/oci"
       configuration_aliases = [oci.home]
-      version               = "~> 4.115.0"
+      version               = ">= 4.115.0"
     }
   }
   required_version = ">= 1.2.0"