c75009584954289b6d1e5a14537e925b6b578596
Devon Crouse <devon.crouse@oracle.com>
2023-07-12 20:36:35 -0600

    fix: Disable unused datasources, create NSGs with auto defaults
    Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

--- modules/network/nsg-workers.tf
+++ modules/network/nsg-workers.tf
@@ -7,7 +7,7 @@ locals {
     lookup(local.worker_nsg_config, "create", "auto") == "always",
     alltrue([
       lookup(local.worker_nsg_config, "create", "auto") == "auto",
-      !contains(keys(local.worker_nsg_config), "id"),
+      coalesce(lookup(local.worker_nsg_config, "id", null), "none") == "none",
       var.create_cluster,
     ]),
   ])
