18a09cdd6c100c1437f1fecc7fcfabc5da51aeec
andrei ilas <andrei.ilas@oracle.com>
2023-09-29 09:20:30 +0300

    exit script execution if a command fails

--- modules/extensions/scripts/create_service_account.template.sh
+++ modules/extensions/scripts/create_service_account.template.sh
@@ -2,6 +2,8 @@
 # Copyright 2017, 2021 Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
+set -e
+
 if [ ! -f .sa_completed ]; then
   if [ ${service_account_namespace} != kube-system ]; then
     kubectl create ns ${service_account_namespace}
--- modules/extensions/scripts/install_metricserver.template.sh
+++ modules/extensions/scripts/install_metricserver.template.sh
@@ -2,6 +2,8 @@
 # Copyright 2017, 2021 Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
+set -e
+
 if [ ! -f .metrics_completed ]; then
 
   echo "Installing Metrics Server"
