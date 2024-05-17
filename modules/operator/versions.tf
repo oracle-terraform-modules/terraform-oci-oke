4f785244192e1e92651870367080bf37c468300a
Ali Mukadam <ali.mukadam@oracle.com>
2024-05-17 15:09:25 +1000

    fix: versions file in submodules
    additional fixes:
      - for default bastion os version when using Autonomous
      - disabling bastion addon when using different OS versions
      - operator await should only trigger if bastion is created
    
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>

--- modules/operator/versions.tf
+++ modules/operator/versions.tf
@@ -1,18 +1,13 @@
-# Copyright (c) 2017, 2023 Oracle Corporation and/or its affiliates.
+# Copyright (c) 2017, 2024 Oracle Corporation and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 terraform {
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.3.0"
 
   required_providers {
     cloudinit = {
       source  = "hashicorp/cloudinit"
       version = ">= 2.2.0"
-    }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.9.0"
     }
 
     null = {
@@ -21,8 +16,8 @@ terraform {
     }
 
     oci = {
-      source  = "oracle/oci"
-      version = ">= 4.119.0"
+      source                = "oracle/oci"
+      version               = ">= 4.119.0"
     }
   }
 }
