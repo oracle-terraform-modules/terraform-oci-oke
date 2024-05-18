070929cd79210b25702bdd506c14ddf444f410f5
Ali Mukadam <ali.mukadam@oracle.com>
2024-05-18 22:49:51 +1000

    fix: versions files
    Resolves #924
    
    Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>

--- modules/iam/versions.tf
+++ modules/iam/versions.tf
@@ -1,1 +1,22 @@
-../../versions.tf
+// Filename: d:\dev\github\terraform-oci-oke\modules\iam\versions.tf
+// Path: d:\dev\github\terraform-oci-oke\modules\iam
+// Created Date: Saturday, May 18th 2024, 10:44:21 pm
+// Author: Ali Mukadam
+// 
+// Copyright (c) 2024 Oracle and/or its affiliates
+
+# Copyright (c) 2017, 2024 Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
+
+terraform {
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    oci = {
+      configuration_aliases = [oci.home]
+      source  = "oracle/oci"
+      version = ">= 4.119.0"
+    }
+  }
+}
+
--- modules/network/versions.tf
+++ modules/network/versions.tf
@@ -1,1 +1,19 @@
-../../versions.tf
+# Copyright (c) 2017, 2024 Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
+
+terraform {
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = ">= 2.2.0"
+    }
+
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 4.119.0"
+    }
+  }
+}
+
